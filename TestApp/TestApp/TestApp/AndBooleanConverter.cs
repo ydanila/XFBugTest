e2e7f490cf4207d2736e1bbc90068d437a4d4826
ydanila <ydanila@gmail.com>
2018-03-06 19:19:53 +0300

    Null reference test app

---
+++ TestApp/TestApp/TestApp/AndBooleanConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using MultiBindingExample;
+
+namespace TestApp
+{
+	public class AndBooleanConverter : IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+		{
+			var result = true;
+
+			foreach(var val in values)
+			{
+				if(val == null)
+				{
+					//	Binding.DoNothing
+					//	DependencyProperty.UnsetValue 
+					return null;
+				}
+
+				if(val is IConvertible)
+				{
+					result = result && ((IConvertible)val).ToBoolean(CultureInfo.CurrentCulture);
+				}
+				else
+				{
+					throw new ArgumentException("Not boolean values not supported");
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ TestApp/TestApp/TestApp/IMultiValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace MultiBindingExample
+{
+	public interface IMultiValueConverter
+	{
+		object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+	}
+}
